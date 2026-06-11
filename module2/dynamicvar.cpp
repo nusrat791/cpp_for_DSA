e12b12925e4c17367c5eb6c05067f9c94f01fa9b
Nusrat Jahan <nusratjahantithi791@gmail.com>
2026-06-11 13:53:09 +0600

    Add files via upload

---
+++ module2/dynamicvar.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int *p;
+void fun()
+{
+
+    int x=10;
+    return;
+}
+int main()
+{
+
+int x=110;
+int *p=new int;
+*p=10;
+cout<<*p<<endl;
+
+
+    return 0;
+}
